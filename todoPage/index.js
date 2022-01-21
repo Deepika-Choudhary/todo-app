91e43fe380092ce31e0ca4d332ce88325eef399c
Deepika-Choudhary <66372106+Deepika-Choudhary@users.noreply.github.com>
2022-01-21 19:51:02 +0530

    Update index.js

--- todoPage/index.js
+++ todoPage/index.js
@@ -2,16 +2,27 @@ console.log("Todo Page")
 
 // add function 
 function add(){
-    console.log('button clicked')
-    // document.getElementById("added").innerHTML += 'added';
-    // document.getElementById("added").className += 'task';
-    document.getElementById("added").classList.add('task');
+    // console.log('add button clicked')
+    document.getElementById("added").innerHTML += `<div class="task">
+        <input type="text" placeholder="New task...">
+            <i class="fa fa-minus-circle fa-3x removeButton" aria-hidden="true" onclick="remove()"></i>
+            <div id="removed"></div>
+   
+</div>`; 
     
 }
 
+// delete function
 function remove(){
-    console.log('remove button clicked');
-    document.getElementById("removed").classList.remove('task');
+    // console.log('remove button clicked');
+    var deleted = document.getElementsByClassName('removeButton');
+    var i;
+    for(i=0;i<deleted.length;i++){
+        deleted[i].onclick = function(){
+            var div = this.parentElement;
+            div.style.display='none';
+        }
+    }
 }
 
-// delete function 
+ 
